e1b3944bf2f6afe534293d2d7200353ddd45d0d9
Aryan Singh <90302042+meltedhyperion@users.noreply.github.com>
2023-07-05 19:21:38 +0530

    Add Database connection services (#4)
    * :sparkles: Add mongodb connection
    
    * :sparkles: Add get all db collections service
    
    * :sparkles: Add check collection exists service
    
    * :sparkles: Add get db collection service
    
    * :memo: Add env example
    
    * :technologist: Update env sample
    
    * :pushpin: move mongo driver dependency to direct

---
+++ database/database.go
@@ -0,0 +1,94 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var DB *mongo.Client
+
+func GetConnection() (*mongo.Client, error) {
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Error Loading .env file")
+		}
+	}
+
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		log.Println("Mongo URI Required")
+	}
+
+	var err error
+
+	if DB == nil {
+		DB, err = mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	}
+
+	if err != nil {
+		panic("Error while connecting with DB." + err.Error())
+	}
+	err = DB.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		panic("Error while connecting with DB." + err.Error())
+	}
+	return DB, nil
+}
+
+func GetAllCollections(dbName string) ([]string, error) {
+	client, err := GetConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	db := client.Database(dbName)
+	collectionNames, err := db.ListCollectionNames(context.Background(), bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	return collectionNames, nil
+}
+
+func CollectionExists(dbName string, collectionName string) (bool, error) {
+
+	collectionNames, err := GetAllCollections(dbName)
+	if err != nil {
+		return false, err
+	}
+
+	for _, name := range collectionNames {
+		if name == collectionName {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func GetCollection(dbName string, collectionName string) (*mongo.Collection, error) {
+	client, err := GetConnection()
+	if err != nil {
+		panic("Error while getting collection " + collectionName + " : " + err.Error())
+	}
+
+	exists, err := CollectionExists(dbName, collectionName)
+	if err != nil {
+		panic("Error while getting collection " + collectionName + " : " + err.Error())
+	}
+
+	if !exists {
+		panic("Collection " + collectionName + " does not exists")
+	}
+
+	collection := client.Database(dbName).Collection(collectionName)
+
+	return collection, nil
+}
