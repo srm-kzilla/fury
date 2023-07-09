04cc68726dc234a410076227bb0bd9ed675a8285
Pradyumna D <pradyumnad092004@gmail.com>
2023-07-09 13:45:00 +0530

    WIP:Add scss files

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,46 +1,21 @@
-import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
-import stylesHref from "../styles/Home.css";
+import type {LinksFunction, V2_MetaFunction} from "@remix-run/node";
+import stylesHref from "../styles/shared/components/Footer.css";
 
 export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        {title: "New Remix App"},
+        {name: "description", content: "Welcome to Remix!"},
+    ];
 };
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesHref },
+    {rel: "stylesheet", href: stylesHref},
 ];
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
 }
