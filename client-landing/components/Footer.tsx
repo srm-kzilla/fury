27054488f2df968c607ea197f0afe141897aef38
CampVamp <ajayramsaravanan4@gmail.com>
2023-07-14 23:40:07 +0530

    :lipstick: Fix UI

--- client-landing/components/Footer.tsx
+++ client-landing/components/Footer.tsx
@@ -5,13 +5,18 @@ import FaceBookLineIcon from "remixicon-react/FacebookCircleLineIcon";
 import MailLine from "remixicon-react/MailLineIcon";
 import LinkedInBoxLine from "remixicon-react/LinkedinBoxLineIcon";
 import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 const Footer = () => {
   return (
-    <div className=" px-16 py-10 bg-recruitments-footerbg">
+    <div className={` px-16 py-6 bg-recruitments-footerbg ${inter.className}`}>
       <div className=" border-b-2 flex gap-10 py-4">
         <div className=" flex flex-col gap-6">
-          <div className=" flex md:gap-4 lg:gap-8 border-b-2 w-3/5">
+          <div className=" flex md:gap-4 lg:gap-8 border-b-2 w-3/5 pb-4">
             <TwitterLineIcon size={40} />
             <InstagramLineIcon size={40} />
             <YoutubeLineIcon size={40} />
@@ -21,7 +26,7 @@ const Footer = () => {
           </div>
           <div className=" flex basis-2/3 gap-10 py-6">
             <div className=" flex flex-col gap-3">
-              <h2 className=" text-recruitments-orange font-semibold text-xl">
+              <h2 className=" text-recruitments-orange font-semibold lg:text-lg">
                 More About Us
               </h2>
               <ul className="">
@@ -30,7 +35,7 @@ const Footer = () => {
               </ul>
             </div>
             <div className=" flex flex-col gap-3">
-              <h2 className=" text-recruitments-orange font-semibold text-xl">
+              <h2 className=" text-recruitments-orange font-semibold lg:text-lg">
                 Related Information
               </h2>
               <ul className="">
@@ -38,7 +43,7 @@ const Footer = () => {
               </ul>
             </div>
             <div className=" flex flex-col gap-3">
-              <h2 className=" text-recruitments-orange font-semibold text-xl">
+              <h2 className=" text-recruitments-orange font-semibold lg:text-lg">
                 Equal Opportunity
               </h2>
               <p>
@@ -53,7 +58,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center basis-1/3">
+        <div className="flex flex-col items-center justify-center basis-1/3">
           <div className=" relative w-64 h-64">
             <Image src="/FooterLogo.svg" alt="FooterLogo" fill />
           </div>
