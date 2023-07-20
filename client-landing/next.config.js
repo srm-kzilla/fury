9e7663a981d9a49deeed5b5cecc1883b56b64210
shawshankkumar <shashankkumarthakur@gmail.com>
2023-07-20 21:27:35 +0530

    chore: redirect rule and delete hello api

--- client-landing/next.config.js
+++ client-landing/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:slug([a-zA-Z0-9]+)', 
+        destination: '/', 
+        permanent: true,
+      },
+      {
+        source: '/:slug1/:slug2*', 
+        destination: '/', 
+        permanent: true,
+      }
+    ];
+  },
 }
 
 module.exports = nextConfig
