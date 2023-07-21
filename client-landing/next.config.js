634dd5eff4c86950296599dde01f7221e95943d7
Mohamed Sami <srmf2025@gmail.com>
2023-07-21 21:02:20 +0530

    :bug: tempfix: remove redirects

--- client-landing/next.config.js
+++ client-landing/next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:slug([a-zA-Z0-9]+)', 
-        destination: '/', 
-        permanent: false,
-      },
-      {
-        source: '/:slug1/:slug2*', 
-        destination: '/', 
-        permanent: false,
-      }
-    ];
-  },
 }
 
 module.exports = nextConfig
