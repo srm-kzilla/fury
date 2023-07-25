1d284da789372a912a28adb83330261b11ad4339
shawshankkumar <shashankkumarthakur@gmail.com>
2023-07-26 00:00:56 +0530

    chore: remove redirect rule

--- client-landing/next.config.js
+++ client-landing/next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:slug([a-zA-Z0-9]+(?<!\\.[a-zA-Z0-9]+))',
-        destination: '/', 
-        permanent: false,
-      },
-      {
-        source: '/:slug1/:slug2*',
-        destination: '/', 
-        permanent: false,
-      }
-    ];
-  },
+  reactStrictMode: true
 }
 
 module.exports
