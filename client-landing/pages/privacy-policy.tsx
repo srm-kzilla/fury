c469b03816e5867d5c96f58895b4961564695972
Saanvi Kumar <113087236+SaanviKumar13@users.noreply.github.com>
2023-07-26 00:48:01 +0530

    Privacy Policy (#37)
    * feat: adds privacy policy page
    
    * feat: reduces margin in mobile view
    
    * chore: made required changes

---
+++ client-landing/pages/privacy-policy.tsx
@@ -0,0 +1,243 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Head from "next/head";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="m-5">
+      <Head>
+        <title>Privacy Policy</title>
+        <meta />
+      </Head>
+      <Navbar />
+      <div className="mx-7 p-2 font-sans md:mx-10">
+        <main className="text-base">
+          <h5 className="my-2 text-xs font-bold">
+            Last Updated on 21st July 2023
+          </h5>
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              The Gist
+            </h2>
+            <p>
+              SRMKZILLA will collect certain non-personally identify information
+              about you as you use our sites. We may use this data to better
+              understand our users. We can also publish this data, but the data
+              will be about a large group of users, not individuals.
+            </p>
+
+            <p>
+              We will also ask you to provide personal information, but you'll
+              always be able to opt out. If you give us personal information, we
+              won't do anything evil with it.
+            </p>
+            <p>
+              We can also use cookies, but you can choose not to store these.
+            </p>
+            <p>
+              That's the basic idea, but you must read through the entire
+              Privacy Policy below and agree with all the details before you use
+              any of our sites.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">Reuse</h2>
+            <p>
+              SRMKZILLA ("SRMKZILLA") makes available services include our
+              websites (
+              <a
+                className="text-highlight"
+                href="https://recruitment.srmkzilla.net"
+              >
+                https://recruitment.srmkzilla.net
+              </a>
+              ), our blog, our API, and any other software, sites, and services
+              offered by SRMKZILLA in connection to any of those (taken
+              together, the "Service"). It is SRMKZILLA's policy to respect your
+              privacy regarding any information we may collect while operating
+              our websites.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Questions
+            </h2>
+            <p>
+              If you have question about this Privacy Policy, please contact us
+              at{" "}
+              <a
+                className="text-highlight"
+                href="mailto:technical@srmkzilla.net"
+              >
+                technical@srmkzilla.net
+              </a>
+              .
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Visitors
+            </h2>
+            <p>
+              Like most website operators, SRMKZILLA collects
+              non-personally-identifying information of the sort that web
+              browsers and servers typically make available, such as the browser
+              type, language preference, referring site, and the date and time
+              of each visitor request. SRMKZILLA's purpose in collecting
+              non-personally identifying information is to better understand how
+              SRMKZILLA's visitors use its website. From time to time, SRMKZILLA
+              may release non-personally-identifying information in the
+              aggregate, e.g., by publishing a report on trends in the usage of
+              its website.
+            </p>
+            <p>
+              SRMKZILLA also collects potentially personally-identifying
+              information like Internet Protocol (IP) addresses. SRMKZILLA does
+              not use such information to identify its visitors, however, and
+              does not disclose such information, other than under the same
+              circumstances that it uses and discloses personally-identifying
+              information, as described below. We may also collect and use IP
+              addresses to block users who violated our Terms of Service.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Gathering of Personally-Identifying Information
+            </h2>
+            <p>
+              Certain visitors to SRMKZILLA's websites choose to interact with
+              SRMKZILLA in ways that require SRMKZILLA to gather
+              personally-identifying information. The amount and type of
+              information that SRMKZILLA gathers depends on the nature of the
+              interaction. SRMKZILLA collects such information only insofar as
+              is necessary or appropriate to fulfill the purpose of the
+              visitor's interaction with SRMKZILLA. SRMKZILLA does not disclose
+              personally-identifying information other than as described below.
+              And visitors can always refuse to supply personally-identifying
+              information, with the caveat that it may prevent them from
+              engaging in certain Service-related activities.
+            </p>
+            <p>
+              Additionally, some interactions, such as posting a comment, may
+              ask for optional personal information. For instance, when posting
+              a comment, may provide a website that will be displayed along with
+              a user's name when the comment is displayed. Supplying such
+              personal information is completely optional and is only displayed
+              for the benefit and the convenience of the user.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Aggregated Statistics
+            </h2>
+            <p>
+              SRMKZILLA may collect statistics about the behavior of visitors to
+              the Service. For instance, SRMKZILLA may monitor the most popular
+              parts of the https://recruitments.srmkzilla.net. SRMKZILLA may
+              display this information publicly or provide it to others.
+              However, SRMKZILLA does not disclose personally-identifying
+              information other than as described below.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Protection of Certain Personally-Identifying Information
+            </h2>
+            <p>
+              SRMKZILLA discloses potentially personally-identifying and
+              personally-identifying information only to those of its employees,
+              contractors and affiliated organizations that (i) need to know
+              that information in order to process it on SRMKZILLA's behalf or
+              to provide services available at SRMKZILLA's websites, and (ii)
+              that have agreed not to disclose it to others. Some of those
+              employees, contractors and affiliated organizations may be located
+              outside of your home country; by using the Service, you consent to
+              the transfer of such information to them. SRMKZILLA will not rent
+              or sell potentially personally-identifying and
+              personally-identifying information to anyone. Other than to its
+              employees, contractors and affiliated organizations, as described
+              above, SRMKZILLA discloses potentially personally-identifying and
+              personally-identifying information only when required to do so by
+              law, or when SRMKZILLA believes in good faith that disclosure is
+              reasonably necessary to protect the property or rights of
+              SRMKZILLA, third parties or the public at large. If you are a
+              registered user of the Service and have supplied your email
+              address, SRMKZILLA may occasionally send you an email to tell you
+              about new features, solicit your feedback, or just keep you up to
+              date with what's going on with SRMKZILLA and our products. We
+              primarily use our website and blog to communicate this type of
+              information, so we expect to keep this type of email to a minimum.
+              If you send us a request (for example via a support email or via
+              one of our feedback mechanisms), we reserve the right to publish
+              it in order to help us clarify or respond to your request or to
+              help us support other users. SRMKZILLA takes all measures
+              reasonably necessary to protect against the unauthorized access,
+              use, alteration or destruction of potentially
+              personally-identifying and personally-identifying information.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Cookies
+            </h2>
+            <p>
+              A cookie is a string of information that a website stores on a
+              visitor's computer, and that the visitor's browser provides to the
+              Service each time the visitor returns. SRMKZILLA uses cookies to
+              help SRMKZILLA identify and track visitors, their usage of
+              SRMKZILLA Service, and their Service access preferences. SRMKZILLA
+              visitors who do not wish to have cookies placed on their computers
+              should set their browsers to refuse cookies before using
+              SRMKZILLA's websites, with the drawback that certain features of
+              SRMKZILLA's websites may not function properly without the aid of
+              cookies.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Data Storage
+            </h2>
+            <p>
+              SRMKZILLA uses third party vendors and hosting partners to provide
+              the necessary hardware, software, networking, storage, and related
+              technology required to run the Service. You understand that
+              although you retain full rights to your data, it may be stored on
+              third party storage and transmitted through third party networks.
+            </p>
+          </div>
+
+          <div className="pb-4">
+            <h2 className="text-highlight text-3xl my-4 font-medium">
+              Privacy Policy Changes
+            </h2>
+            <p>
+              Although most changes are likely to be minor, SRMKZILLA may change
+              its Privacy Policy from time to time, and in SRMKZILLA's sole
+              discretion. SRMKZILLA encourages visitors to frequently check this
+              page for any changes to its Privacy Policy. Your continued use of
+              this site after any change in this Privacy Policy will constitute
+              your acceptance of such change. SRMKZILLA uses third-party vendors
+              and hosting partners to provide the necessary hardware, software,
+              networking, storage, and related technology required to run the
+              Service. You understand that although you retain full rights to
+              your data, it may be stored on third-party storage and transmitted
+              through third-party networks.
+            </p>
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
