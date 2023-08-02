d2ff60d31d1cd03917c46f4194b10946fe2d8a56
Aryan Singh <90302042+meltedhyperion@users.noreply.github.com>
2023-08-02 12:53:50 +0530

    Add User Authentication Middleware (#61)
    * :label: update Contact type
    
    * :sparkles: Add user authentication middleware
    
    * :zap: Add Bearer check

--- constants/constants.go
+++ constants/constants.go
@@ -2,3 +2,4 @@ package constants
 
 const GoogleUserInfoApi = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 const SrmEmailDomain = "@srmist.edu.in"
+const GoogleAccessTokenInfoApi = "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token="
---
+++ middlewares/middlewares.go
@@ -0,0 +1,64 @@
+package middlewares
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/srm-kzilla/Recruitments/constants"
+)
+
+type GoogleAccessTokenInfo struct {
+	Exp       string `json:"exp"`
+	Email     string `json:"email"`
+	ExpiresIn string `json:"expires_in"`
+}
+
+func UserAuthenticate(c *fiber.Ctx) error {
+	accessToken := c.GetReqHeaders()["Authorization"]
+	if accessToken == "" {
+		return c.Status(401).JSON(fiber.Map{
+			"message": "Authorization token not found",
+		})
+	}
+	if !strings.Contains(accessToken, "Bearer") {
+		return c.Status(401).JSON(fiber.Map{
+			"message": "Invalid token",
+		})
+	}
+	token := strings.Split(accessToken, " ")[1]
+	err := getGoogleAccessTokenInfo(token)
+	if err != nil {
+		return c.Status(401).JSON(fiber.Map{
+			"message": "Invalid token",
+		})
+	}
+	return c.Next()
+}
+
+func getGoogleAccessTokenInfo(accessToken string) error {
+	var tokenInfo GoogleAccessTokenInfo
+	res, err := http.Get(constants.GoogleAccessTokenInfoApi + accessToken)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(&tokenInfo)
+	if err != nil {
+		return err
+	}
+	exp, err := strconv.Atoi(tokenInfo.Exp)
+	if err != nil {
+		return err
+	}
+	if exp < int(time.Now().Unix()) {
+		return errors.New("token expired")
+	}
+	return nil
+
+}
