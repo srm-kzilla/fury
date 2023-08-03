ada14b680897b0bee2fb4e88b9e6ecf325a9ffd9
meltedhyperion <aryan12official34@gmail.com>
2023-08-03 22:33:45 +0530

    :sparkles: Add recaptcha middleware

--- api/utils/constants/constants.go
+++ api/utils/constants/constants.go
@@ -3,3 +3,4 @@ package constants
 const GoogleUserInfoApi = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 const SrmEmailDomain = "@srmist.edu.in"
 const GoogleAccessTokenInfoApi = "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token="
+const GoogleRecaptchaVerifyApi = "https://www.google.com/recaptcha/api/siteverify"
