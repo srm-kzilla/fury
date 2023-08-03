7f0c036bdea69c6458a3338f31a27144ebe0bff9
shawshankkumar <shashankkumarthakur@gmail.com>
2023-08-03 14:23:59 +0530

    fix: google oauth2

--- api/routes/auth.router.go
+++ api/routes/auth.router.go
@@ -6,6 +6,5 @@ import (
 )
 
 func AuthRoutes(router fiber.Router) {
-	router.Get("/google/login", controllers.GoogleLogin)
-	router.Get("/google/token", controllers.GetAccessTokenGoogle)
+	router.Post("/google/token", controllers.GetAccessTokenGoogle)
 }
