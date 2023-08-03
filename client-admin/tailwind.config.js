3c91f252779122ab07c8b881f9bf930cce01d794
ShivamBansal07 <sb2368@srmist.edu.in>
2023-08-04 00:03:41 +0530

    feat: Login page UI

--- client-admin/tailwind.config.js
+++ client-admin/tailwind.config.js
@@ -4,6 +4,25 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors:{
+        'kz-dark-grey':'#151319',
+        'kz-orange':'#e94560',
+        'kz-textblue' : '#4e4feb',
+        'kz-lightblue': '#569ed6',
+        'kz-deepblue' : '#3b3ba6',
+      },
+      boxShadow:{
+        'box-shadow': '0px 3px 16px rgba(255, 95, 95, 0.24)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
+      fontFamily:{
+        josefinSans: ['Josefin Sans', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      }
+    },    
+  },
   plugins: [], 
 }
+ 
