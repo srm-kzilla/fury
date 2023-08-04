454b99c67836b30d904f80df1bb99057cfec82e6
Saanvi Kumar <113087236+SaanviKumar13@users.noreply.github.com>
2023-08-04 12:20:52 +0530

    feat: headbar (#85)
    * feat: adds headline to index
    
    * feat: removes markdown package
    
    * chore: adds link and made requested changes
    
    * fix: aligning text in headline

---
+++ client/landing/components/Headbar.tsx
@@ -0,0 +1,19 @@
+const Headbar = () => {
+  return (
+    <div className="p-1 pb-1 flex items-center text-center justify-center text-sm font-body font-light z-50 bg-header-img ">
+      <p>
+        Dear Freshers, we see you can&apos;t wait. Recruitments are now open!
+        :&#41;{" "}
+        <a
+          href="https://photos.google.com/share/AF1QipORRPNrrr85gqjyS5pE43TsBKHW8PxfwUJ3hKXAgdv0SxSTKeExuMFM9RiCbIPNLQ?key=UUtMem5BOS1LeF9kYU1MWmlYRk05eGhZVVJRYXRn"
+          className="underline decoration-dashed"
+        >
+          {" "}
+          #2022Wrapped
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Headbar;
--- client/landing/pages/index.tsx
+++ client/landing/pages/index.tsx
@@ -5,6 +5,7 @@ import Roadmap from "@/components/Roadmap";
 import CoreValues from "@/components/CoreValues";
 import Footer from "@/components/Footer";
 import Head from "next/head";
+import Headbar from "@/components/Headbar";
 
 export default function Home() {
   return (
@@ -26,24 +27,27 @@ export default function Home() {
         <meta name="language" content="English" />
         <meta name="revisit-after" content="10 days" />
         <meta name="author" content="SRMKZILLA" />
-        <meta
-          name="copyright"
-          content="All rights reserved,2023. SRMKZILLA"
-        />
+        <meta name="copyright" content="All rights reserved,2023. SRMKZILLA" />
         <meta httpEquiv="content-language" content="en" />
         <meta name="application-name" content="SRMKZILLA" />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://recruitment.srmkzilla.net/" />
-        <meta property="og:image" content="https://recruitment-2021.s3.ap-south-1.amazonaws.com/life/Screenshot+from+2021-09-14+17-35-58.png" />
+        <meta
+          property="og:image"
+          content="https://recruitment-2021.s3.ap-south-1.amazonaws.com/life/Screenshot+from+2021-09-14+17-35-58.png"
+        />
         <meta
           property="og:description"
           content="SRMKZILLA is a community of young tech enthusiasts who eat, sleep and breath technology. We organize everything from technical workshops to gaming events, you name it & we do it. "
         />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://recruitment.srmkzilla.net/" />
+        <meta
+          property="twitter:url"
+          content="https://recruitment.srmkzilla.net/"
+        />
         <meta property="twitter:site" content="@SRMKZILLA_Club" />
 
         <meta
@@ -54,13 +58,14 @@ export default function Home() {
 
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#000" />
-    </Head>
-    <Navbar />
-    <Hero />
-    <Life />
-    <Roadmap />
-    <CoreValues />
-    <Footer />
+      </Head>
+      <Headbar />
+      <Navbar />
+      <Hero />
+      <Life />
+      <Roadmap />
+      <CoreValues />
+      <Footer />
     </>
   );
 }
