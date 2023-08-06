189283fa59fa350e4a410c1c0308913e3268a1ff
shawshankkumar <shashankkumarthakur@gmail.com>
2023-08-06 15:43:36 +0530

    fix: minor fixes to auth

--- api/routes/auth.router.go
+++ api/routes/auth.router.go
@@ -7,7 +7,7 @@ import (
 
 func AuthRoutes(router fiber.Router) {
 	google := router.Group("/google/")
-	//google.Get("/login", controllers.GoogleLogin)
+	google.Get("/login", controllers.GoogleLogin)
 	google.Post("/token", controllers.GetAccessTokenGoogle)
 	google.Get("/refresh", controllers.RefreshAccessTokenGoogle)
 }
