1be4255cbf87ced12842cfed2f18ff37d1dfa6d6
Saanvi Kumar <113087236+SaanviKumar13@users.noreply.github.com>
2023-08-07 22:07:25 +0530

    feat: Redirects privacy-policy (#126)

--- client/dashboard/app/routes/privacy.tsx
+++ client/dashboard/app/routes/privacy.tsx
@@ -2,6 +2,4 @@ import type { LoaderFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 
 export const loader: LoaderFunction = () =>
-  redirect(
-    "https://gist.github.com/shawshankkumar/faf6f0028e01ae56610dc3b2830a777e"
-  );
+  redirect("https://recruitment.srmkzilla.net/privacy-policy");
