48e77be7cf053a9581ce866c9ddbdc7557139e84
Aryan Singh <90302042+meltedhyperion@users.noreply.github.com>
2023-08-07 21:08:50 +0530

    user notifications api (#121)
    * :sparkles: Add user application api logic
    
    * :sparkles: Add user notifications api logic

--- api/models/user.model.go
+++ api/models/user.model.go
@@ -2,22 +2,24 @@ package models
 
 import (
 	"time"
-    "go.mongodb.org/mongo-driver/bson/primitive"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID          primitive.ObjectID `json:"_id" bson:"_id"`
-	RegNo       string             `validate:"required" json:"regNo" bson:"regNo"`
-	Name        string             `validate:"required" json:"name"`
-	Year        int                `validate:"required" json:"year"`
-	Email       string             `validate:"required" json:"email"`
-	Gender      string             `validate:"required" json:"gender"`
-	Branch      string             `validate:"required" json:"branch"`
-	Resume      string             `validate:"required" json:"resume"`
-	Contact     string             `validate:"required" json:"contact"`
-	CreatedAt   int64              `json:"createdAt" bson:"createdAt"`
-	Application []Application      `json:"application" default:"[]" bson:"application"`
-	Socials     Social             `json:"socials"`
+	ID            primitive.ObjectID `json:"_id" bson:"_id"`
+	RegNo         string             `validate:"required" json:"regNo" bson:"regNo"`
+	Name          string             `validate:"required" json:"name"`
+	Year          int                `validate:"required" json:"year"`
+	Email         string             `validate:"required" json:"email"`
+	Gender        string             `validate:"required" json:"gender"`
+	Branch        string             `validate:"required" json:"branch"`
+	Resume        string             `validate:"required" json:"resume"`
+	Contact       string             `validate:"required" json:"contact"`
+	CreatedAt     int64              `json:"createdAt" bson:"createdAt"`
+	Notifications []Notification     `json:"notifications" bson:"notifications"`
+	Application   []Application      `json:"application" default:"[]" bson:"application"`
+	Socials       Social             `json:"socials"`
 }
 
 type Social struct {
@@ -27,7 +29,13 @@ type Social struct {
 }
 
 type Activity struct {
-	Timestamp      time.Time `json:"timestamp" bson:"timestamp"`
-	Location    string `json:"location" bson:"location"`
-	Type    string `json:"type" bson:"type"`
+	Timestamp time.Time `json:"timestamp" bson:"timestamp"`
+	Location  string    `json:"location" bson:"location"`
+	Type      string    `json:"type" bson:"type"`
+}
+
+type Notification struct {
+	Timestamp time.Time `json:"timestamp" bson:"timestamp"`
+	Markdown  string    `json:"markdown" bson:"markdown"`
+	Text      string    `json:"text" bson:"text"`
 }
