1081cb1c94cb029b698d9ede7743a359d51a4d9c
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-08-09 00:55:28 +0530

    feat: auto updates form

--- client/dashboard/app/routes/start.tsx
+++ client/dashboard/app/routes/start.tsx
@@ -1,7 +1,12 @@
-import { Form, useNavigation, useActionData } from "@remix-run/react";
+import {
+  Form,
+  useNavigation,
+  useActionData,
+  useLoaderData,
+} from "@remix-run/react";
 import { getUserDetails, updateUserDetails } from "~/utils/api.server";
 import userProfileStyles from "~/styles/components/UserProfile.css";
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { BiLoader } from "react-icons/bi";
 import * as Yup from "yup";
 import type {
@@ -22,7 +27,7 @@ export const loader: LoaderFunction = async ({ request }) => {
   const user = await getUserDetails(request);
 
   if (!!user.gender) return redirect("/");
-  return null;
+  return json({ user });
 };
 
 type ActionData = {
@@ -110,7 +115,7 @@ const validateUserDetails = async (formData: FormData) => {
 export default function Start() {
   const navigation = useNavigation();
   const actionData = useActionData<ActionData>();
-
+  const { user } = useLoaderData();
   return (
     <Form method="post" className="kz-user-form" encType="multipart/form-data">
       <div>
@@ -124,6 +129,14 @@ export default function Start() {
       </div>
       <div className="grid-box">
         <div className="select">
+          <label className="label" htmlFor="name">
+            Name<sup>*</sup>
+          </label>
+          <input type="text" value={user.name} readOnly />
+          <label className="label" htmlFor="regno">
+            Registration Number<sup>*</sup>
+          </label>
+          <input type="text" value={user.regNo} readOnly />
           <label className="label" htmlFor="gender">
             Gender<sup>*</sup>
           </label>
