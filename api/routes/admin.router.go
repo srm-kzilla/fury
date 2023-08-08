b06c3a5f819157ce5708edc8b05e26ec2269d639
mdfarhaan <farhaanm110@gmail.com>
2023-08-09 03:16:46 +0530

    feat: add admin middleware

--- api/routes/admin.router.go
+++ api/routes/admin.router.go
@@ -3,19 +3,18 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/srm-kzilla/Recruitments/api/controllers"
+	"github.com/srm-kzilla/Recruitments/api/utils/middlewares"
 )
 
-func AdminRoutes(router fiber.Router){
+func AdminRoutes(router fiber.Router) {
+	protected := router.Use(middlewares.AdminAuthenticate)
+	protected.Post("/signup", controllers.AdminSignup)
+	protected.Post("/login", controllers.AdminLogin)
 
-	router.Post("/signup", controllers.AdminSignup)
-	router.Post("/login", controllers.AdminLogin)
-	
-	applicationsAdmin := router.Group("/applications")
+	applicationsAdmin := protected.Group("/applications")
 
 	applicationsAdmin.Get("/", controllers.GetAllApplications)
 	applicationsAdmin.Get("/:domain", controllers.GetApplications)
 	applicationsAdmin.Put("/", controllers.UpdateApplications)
-
-
 
 }
