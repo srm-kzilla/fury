d8ec15c65959c055937dc984f087d6de65ec8224
YARE0909 <pradyumnad092004@gmail.com>
2023-08-10 10:22:46 +0530

    Fix layout

--- client/dashboard/app/routes/_index.tsx
+++ client/dashboard/app/routes/_index.tsx
@@ -109,7 +109,7 @@ const Dashboard = () => {
               )}
 
               <h2 className="mt">Notifications</h2>
-              <div style={{ overflowX: "hidden" }}>
+              <div className="notif-wrapper">
                 <div className="notif-row">
                   <div className="kz-notifications">
                     {notifications && notifications.map((notification: Notification) => {
