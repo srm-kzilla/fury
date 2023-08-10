f290b56f08f4aa13d27c3c4731640f0ffc4788e9
YARE0909 <pradyumnad092004@gmail.com>
2023-08-10 22:42:32 +0530

    Update content

--- client/dashboard/app/routes/start.tsx
+++ client/dashboard/app/routes/start.tsx
@@ -14,7 +14,7 @@ import type {
   LinksFunction,
   LoaderFunction,
 } from "@remix-run/node";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import toast from "~/utils/toast.client";
 import type { ValidationError } from "yup";
@@ -65,7 +65,7 @@ export const action: ActionFunction = async ({ request }) => {
       return redirect("/");
     }
   } catch (errors) {
-    console.log(errors)
+    console.log(errors);
     return { errors };
   }
 };
@@ -134,7 +134,6 @@ export default function Start() {
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   useEffect(() => {
-
     if (actionData?.toastErrMessage) {
       toast.error(actionData.toastErrMessage);
     }
@@ -148,7 +147,8 @@ export default function Start() {
       <div>
         <h5>
           We like being on a first-name basis, but it also helps us get in touch
-          with you.
+          with you. While linkedin, portfolio and others are not mandatory, it helps your
+          chances
         </h5>
       </div>
       <div className="grid-box">
@@ -156,13 +156,23 @@ export default function Start() {
           <label className="label" htmlFor="name">
             Name<sup>*</sup>
           </label>
-          <input type="text" name="name" defaultValue={name} placeholder="Michael Scott" />
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            placeholder="Michael Scott"
+          />
         </div>
         <div className="select">
           <label className="label" htmlFor="regno">
             Registration Number<sup>*</sup>
           </label>
-          <input type="text" value={regNo} placeholder="RA2211026010111" readOnly />
+          <input
+            type="text"
+            value={regNo}
+            placeholder="RA2211026010111"
+            readOnly
+          />
         </div>
         <div className="select">
           <label className="label" htmlFor="gender">
