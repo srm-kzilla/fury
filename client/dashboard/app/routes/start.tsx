b97c650ce6e51d92a3ee76be17cc695f414a5a79
utkarsh575 <utkarshlohani575@gmail.com>
2023-08-12 20:24:28 +0530

    ::pencil2: fix typo in validation

--- client/dashboard/app/routes/start.tsx
+++ client/dashboard/app/routes/start.tsx
@@ -99,13 +99,13 @@ const validateUserDetails = async (formData: FormData) => {
     linkedin: Yup.string()
       .url("The URL you have entered doesn't seem right")
       .matches(
-        /^https?:\/\/(?:[a-z]+\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]+$/,
+        /^(https?:\/\/(?:[a-z]+\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]+)?$/,
         "The URL you have entered doesn't seem right"
       ),
     github: Yup.string()
       .url("The URL you have entered doesn't seem right")
       .matches(
-        /^https?:\/\/(?:[a-z]+\.)?github\.com\/[a-zA-Z0-9_-]+$/,
+        /^(https?:\/\/(?:[a-z]+\.)?github\.com\/[a-zA-Z0-9_-]+)?$/,
         "The URL you have entered doesn't seem right"
       ),
     resume: Yup.string(),
