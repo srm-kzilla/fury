6ec53f8fafdff9988639d5beeb066fa24fa88802
shawshankkumar <shashankkumarthakur@gmail.com>
2023-08-14 00:50:54 +0530

    fix: temp fix for recaptcha

--- client/dashboard/app/utils/recaptcha.ts
+++ client/dashboard/app/utils/recaptcha.ts
@@ -1,10 +1,10 @@
-import { load } from "recaptcha-v3";
-import getEnv from "./env";
+// import { load } from "recaptcha-v3";
+// import getEnv from "./env";
 
 export default async function getRecaptchaToken(action: string) {
-  const siteKey = getEnv().REACT_APP_RECAPTCHA_SITE_KEY;
-  const recaptcha = await load(siteKey!);
-  const recaptchaToken = await recaptcha.execute(action);
+  // const siteKey = getEnv().REACT_APP_RECAPTCHA_SITE_KEY;
+  // const recaptcha = await load(siteKey!);
+  // const recaptchaToken = await recaptcha.execute(action);
 
-  return recaptchaToken;
+  return "";
 }
