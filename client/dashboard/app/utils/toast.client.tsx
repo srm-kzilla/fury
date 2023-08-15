a0e944d16f7b92e8049889a29bed5c972bf9246a
YARE0909 <pradyumnad092004@gmail.com>
2023-08-15 14:07:03 +0530

    Add close button to toasts

---
+++ client/dashboard/app/utils/toast.client.tsx
@@ -0,0 +1,112 @@
+import toast from "react-hot-toast";
+import { AiOutlineClose } from "react-icons/ai";
+
+export default {
+  show: (message: string, icon: string) =>
+    toast.custom((t) => (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          background: "#111111",
+          color: "#fff",
+          border: "1px solid #30363d",
+          maxWidth: "300px",
+          padding: "1rem",
+          zIndex: "100000",
+          width: "fit-content",
+          borderRadius: "10px",
+          gap: "1rem",
+        }}
+      >
+        {icon}
+        <h1
+          style={{
+            fontSize: "0.8rem",
+          }}
+        >
+          {message}
+        </h1>
+        <h5
+          style={{
+            cursor: "pointer",
+          }}
+          onClick={() => toast.dismiss()}
+        >
+          <AiOutlineClose />
+        </h5>
+      </div>
+    )),
+  error: (message: string) =>
+    toast.error(
+      (t) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            background: "#111111",
+            color: "#fff",
+            border: "1px solid #30363d",
+            maxWidth: "300px",
+            padding: "1rem",
+            zIndex: "100000",
+            width: "fit-content",
+            borderRadius: "10px",
+            gap: "1rem",
+          }}
+        >
+          <h1
+            style={{
+              fontSize: "0.8rem",
+            }}
+          >
+            {message}
+          </h1>
+          <h5
+            style={{
+              cursor: "pointer",
+            }}
+            onClick={() => toast.dismiss()}
+          >
+            <AiOutlineClose />
+          </h5>
+        </div>
+      )
+    ),
+  success: (message: string) =>
+    toast.success(
+      (t) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            background: "#111111",
+            color: "#fff",
+            border: "1px solid #30363d",
+            maxWidth: "300px",
+            padding: "1rem",
+            zIndex: "100000",
+            width: "fit-content",
+            borderRadius: "10px",
+            gap: "1rem",
+          }}
+        >
+          <h1
+            style={{
+              fontSize: "0.8rem",
+            }}
+          >
+            {message}
+          </h1>
+          <h5
+            style={{
+              cursor: "pointer",
+            }}
+            onClick={() => toast.dismiss()}
+          >
+            <AiOutlineClose />
+          </h5>
+        </div>
+      )
+    ),
+};
