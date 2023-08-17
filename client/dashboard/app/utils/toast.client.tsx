c2e3a45df592aa548b1943fcfe28fa9dfd95953c
Mohamed Sami <srmf2025@gmail.com>
2023-08-18 00:15:02 +0530

    fix: revert error and success toast styles

--- client/dashboard/app/utils/toast.client.tsx
+++ client/dashboard/app/utils/toast.client.tsx
@@ -1,6 +1,17 @@
 import toast from "react-hot-toast";
 import { AiOutlineClose } from "react-icons/ai";
 
+const toastBaseOptions = {
+  style: {
+    background: "#111111",
+    color: "#fff",
+    border: "1px solid #30363d",
+    maxWidth: "300px",
+    padding: "1rem",
+    zIndex: "100000",
+  },
+};
+
 export default {
   show: (message: string, icon: string) =>
     toast.custom((t) => (
@@ -37,76 +48,6 @@ export default {
         </h5>
       </div>
     )),
-  error: (message: string) =>
-    toast.error(
-      (t) => (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            background: "#111111",
-            color: "#fff",
-            border: "1px solid #30363d",
-            maxWidth: "300px",
-            padding: "1rem",
-            zIndex: "100000",
-            width: "fit-content",
-            borderRadius: "10px",
-            gap: "1rem",
-          }}
-        >
-          <h1
-            style={{
-              fontSize: "0.8rem",
-            }}
-          >
-            {message}
-          </h1>
-          <h5
-            style={{
-              cursor: "pointer",
-            }}
-            onClick={() => toast.dismiss()}
-          >
-            <AiOutlineClose />
-          </h5>
-        </div>
-      )
-    ),
-  success: (message: string) =>
-    toast.success(
-      (t) => (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            background: "#111111",
-            color: "#fff",
-            border: "1px solid #30363d",
-            maxWidth: "300px",
-            padding: "1rem",
-            zIndex: "100000",
-            width: "fit-content",
-            borderRadius: "10px",
-            gap: "1rem",
-          }}
-        >
-          <h1
-            style={{
-              fontSize: "0.8rem",
-            }}
-          >
-            {message}
-          </h1>
-          <h5
-            style={{
-              cursor: "pointer",
-            }}
-            onClick={() => toast.dismiss()}
-          >
-            <AiOutlineClose />
-          </h5>
-        </div>
-      )
-    ),
+  error: (message: string) => toast.error(message, toastBaseOptions),
+  success: (message: string) => toast.success(message, toastBaseOptions),
 };
