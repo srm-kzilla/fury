e2fb69a8620e6be2a9d208d45f38e6d7148c350d
Utkarshini <utkarshininarayan@gmail.com>
2023-08-19 20:26:00 +0530

    refactor: format the questions

--- client/dashboard/app/utils/applications.ts
+++ client/dashboard/app/utils/applications.ts
@@ -99,7 +99,7 @@ export const questionsArray: Array<
     },
     {
       domain: "gfx",
-      question: `Did you see our event poster for ? If you haven't, swiftly visit our Instagram page and check it out (https://www.instagram.com/p/Cv96ZrEvVgf/). We hope you liked it but can you make it better?`,
+      question: `Did you see our event poster for "Off the Clock"? If you haven't, swiftly visit our Instagram page and check it out <a href=https://www.instagram.com/p/Cv96ZrEvVgf/ target=_blank><i><u>Off the Clock</u></i> </a>.<br/> We hope you liked it but can you make it better?`,
     },
     {
       domain: "vfx",
@@ -107,7 +107,7 @@ export const questionsArray: Array<
     },
     {
       domain: "corporate",
-      question: `How do you think you would be able to contribute to the club and this domain and how do you think the corporate domain works within a club?`,
+      question: `How do you think you would be able to contribute to the club and this domain? How do you think the corporate domain works within a club?`,
     },
     {
       domain: "events",
@@ -126,16 +126,11 @@ export const questionsArray: Array<
     },
     {
       domain: "gfx",
-      question: `Suggest edits to our Multiverse of IoT poster and give it your awesome touch (instagram: https://www.instagram.com/p/Cd8b7GMPZ_Y/?utm_source=ig_web_copy_link)`,
+      question: `Suggest edits to our Multiverse of IoT poster and give it your awesome touch (instagram: <a href=https://www.instagram.com/p/Cd8b7GMPZ_Y/?utm_source=ig_web_copy_link target=_blank><u>Multiverse of IoT</u></a>)`,
     },
     {
       domain: "vfx",
       question: `How will you deal with a software mismatch when working with a team (for example: After Effects and Premier Pro)? What is your go-to tool for making a video?      `,
-    },
-    {
-      domain: "photography",
-      question:
-        "Which software do you use for editing images and videos? How proficient are you with these tools?",
     },
     {
       domain: "photography",
@@ -149,7 +144,7 @@ export const questionsArray: Array<
     {
       domain: "events",
       question:
-        "Do you have any prior experience in event planning or event management? If yes, we would love to know! If not, that’s okay too :)",
+        "Do you have any prior experience in event planning or event management? <br/>If yes, we would love to know! If not, that’s okay too :)",
     },
   ],
   [
@@ -198,10 +193,10 @@ export const questionsArray: Array<
     },
     {
       domain: "content_writing",
-      question: `How would you increase SRMKZILLA's social media engagement for a product launch? (via stories/reels/posts) Head to our Instagram page and check out the posts on the update of our URL shrinker; Kzilla XYZ 2.0.
-      <br/>      Post 1: https://www.instagram.com/p/CklX_Mjqufg/?img_index=1
+      question: `How would you increase SRMKZILLA's social media engagement for a product launch? (via stories/reels/posts) <br/>Head to our Instagram page and check out the posts on the update of our URL shrinker <i>Kzilla XYZ 2.0</i>.
+      <br/>      Post 1: <a href=https://www.instagram.com/p/CklX_Mjqufg/?img_index=1 target=_blank><u>Kzilla XYZ Post</u> </a>
       <br/>
-      Post 2 (reel): https://www.instagram.com/p/Ck3l51Wjo4s/
+      Post 2: <a href=https://www.instagram.com/p/Ck3l51Wjo4s/ target=_blank><u>Kzilla XYZ Reel</u></a>
       <br/>
       Suggest ideas that can be used in the posts/videos/reels/story for the launch of the update. (put your endless scrolling through social media to good use xD)
         
@@ -240,17 +235,17 @@ export const questionsArray: Array<
       domain: "content_writing",
       question: ` This task is mandatory for 2nd and 3rd-year students however we strongly recommend 1st-year students to give it a shot too.
       <br/>
-      How would you entice people to check out the club event sponsors using social media posts? Check out our insta posts for our Mozofest 23 Sponsors, Devfolio and Bikaji, and write some quick, interesting content for them!
+      How would you entice people to check out the club event sponsors using social media posts? Check out our insta posts for our <i>Mozofest'23</i> Sponsors, <i>Devfolio and Bikaji</i>, and write some quick, interesting content for them!
       <br/>
-      Devfolio: https://www.instagram.com/p/CpCqYkCvSN9/
+      Link 1: <a href=https://www.instagram.com/p/CpCqYkCvSN9/  target=_blank><u>Devfolio</u></a>
       <br/>
-      Bikaji: https://www.instagram.com/p/CpCq-R3PXVY/
+      Link 2: <a hrefhttps://www.instagram.com/p/CpCq-R3PXVY/ target=_blank><u>Bikaji</u></a>
       `,
     },
     {
       domain: "gfx",
       question: `In Addition, What are the details that you would take into account for the posts of the series? You can use our series on Web 3 as a reference point
-      https://www.instagram.com/p/CgE0GV-vCFQ/?igshid=MzRlODBiNWFlZA==
+      <a href=https://www.instagram.com/p/CgE0GV-vCFQ/?igshid=MzRlODBiNWFlZA== target=_blank><u>The WEB3 Chronicles<u> </a>
       `,
     },
     {
@@ -259,19 +254,19 @@ export const questionsArray: Array<
     },
     {
       domain: "corporate",
-      question: `After mutually agreeing to a sponsorship for an upcoming event, a sponsor backs out at the last minute. How would you as a member of the corporate domain handle this situation?`,
+      question: `After mutually agreeing to a sponsorship for an upcoming event, a sponsor backs out at the last minute. <br/>How would you as a member of the corporate domain handle this situation?`,
     },
     {
       domain: "events",
       question:
-        "You are given paper cups, ping pong balls, threads, blindfold cloth, darts, and a rectangular wooden board. Design a mini game using these materials that would not take more than 20 minutes to finish.",
+        "You are given paper cups, ping pong balls, threads, blindfold cloth, darts, and a rectangular wooden board. <br/>Design a mini game using these materials that would not take more than 20 minutes to finish.",
     },
   ],
   [
     {
       domain: "technical",
       question:
-        "Are you currently working on any projects? if yes, what is the tech stack, and why did you choose that particular tech stack if not, what technologies are you looking forward to learning in the near future? (If the project is open source, give us the link! not mandatory but has brownie points)",
+        "Are you currently working on any projects?<br/> If yes, what is the tech stack, and why did you choose that particular tech stack? <br>If not, what technologies are you looking forward to learning in the near future? <br/>(If the project is open source, give us the link! not mandatory but has brownie points)",
     },
     {
       domain: "photography",
@@ -288,7 +283,7 @@ export const questionsArray: Array<
     },
     {
       domain: "vfx",
-      question: `What kind of social media content excites Millennials/ Gen Z? How can a technical club-like SRMKZILLA implement this kind of content while staying true to its technical roots?`,
+      question: `What kind of social media content excites <i>Millennials/ Gen Z</i> ? How can a technical club-like <i>SRMKZILLA</i> implement this kind of content while staying true to its technical roots?`,
     },
     {
       domain: "corporate",
@@ -297,7 +292,7 @@ export const questionsArray: Array<
     {
       domain: "events",
       question:
-        "Suppose you planned to organize a workshop in a hall with a capacity of 200 people, but you found out that only 120 people turned up out of 500 registrations. How would you react to this situation and what actions would you take?",
+        "Suppose you planned to organize a workshop in a hall with a capacity of 200 people, but you found out that only 120 people turned up out of 500 registrations.<br/> How would you react to this situation and what actions would you take?",
     },
   ],
   [
@@ -313,11 +308,11 @@ export const questionsArray: Array<
     },
     {
       domain: "content_writing",
-      question: `What kind of social media content excites Millennials/ Gen Z? How can a technical club-like SRMKZILLA implement this kind of content while staying true to its technical roots?`,
+      question: `What kind of social media content excites <i>Millennials/ Gen Z?</i> How can a technical club-like <i>SRMKZILLA</i> implement this kind of content while staying true to its technical roots?`,
     },
     {
       domain: "gfx",
-      question: `If you had the freedom to redesign the landing page of our recruitment portal what design changes would you make? link: https://recruitment.srmkzilla.net
+      question: `If you had the freedom to redesign the landing page of our recruitment portal, what design changes would you make? link: <a href=https://recruitment.srmkzilla.net target=_blank><u>Recruitment portal</u></a>
 `,
     },
     {
@@ -326,12 +321,12 @@ export const questionsArray: Array<
     },
     {
       domain: "corporate",
-      question: `Scenario based question: You have been told to send out mails to potential sponsors for your upcoming event and the only information you have is the date and the agenda of the event. How would you draft a mail to the potential sponsor convincing them to sponsor our event despite only having minimal details?(Draft a mail below)`,
+      question: `Scenario based question: You have been told to send out mails to potential sponsors for your upcoming event and the only information you have is the date and the agenda of the event.<br/> How would you draft a mail to the potential sponsor convincing them to sponsor our event despite only having minimal details? (Draft a mail below)`,
     },
     {
       domain: "events",
       question:
-        "The venue for your upcoming event faces unforeseen issues, forcing you to relocate the event just two days before the date. How would you manage this change and communicate it to attendees and stakeholders effectively?",
+        "The venue for your upcoming event faces unforeseen issues, forcing you to relocate the event just two days before the date.<br/> How would you manage this change and communicate it to attendees and stakeholders effectively?",
     },
   ],
   [
@@ -352,28 +347,28 @@ export const questionsArray: Array<
     {
       domain: "gfx",
       question: `Note: This task is mandatory for 2nd and 3rd-year students however we strongly recommend 1st-year students to give it a shot too.
-
-      Task 1: Design an event poster
-        a. Topic: Designathon
-        b. Required fields:
-            i. Social Media footer
-            ii. Date/Time
-            iii. Registration link
-            iv. Custom heading/Event logo
-        c. Size: 1080px by 1080px
-        d. Export: JPEG format with the source file
-        e. Bonus: To create greater impact, experiment with typography
-      
-      Task 2: Illustration(Adobe Illustrator, Affinity Designer, sketch)
-        a. Topic: Redesign the SRMKZILLA logo
-        b. Size: 1080px by 1080px
-        c. Export: JPEG format with the source file
-        d. Bonus: Sign off your artwork with your own custom logo
-      
-      Task 3: UI/UX (Figma, Sketch)
-        a. Topic: Redesign RECRUITMENT homepage
-        b. Export: JPEG format with the source file
-        c. Bonus: Try to include gradient effect in elements.
+      <br/>
+      Task 1: Design an event poster<br/>
+        a. Topic: Designathon<br/>
+        b. Required fields:<br/>
+            i. Social Media footer<br/>
+            ii. Date/Time<br/>
+            iii. Registration link<br/>
+            iv. Custom heading/Event logo<br/>
+        c. Size: 1080px by 1080px<br/>
+        d. Export: JPEG format with the source file<br/>
+        e. Bonus: To create greater impact, experiment with typography<br/>
+      <br/>
+      Task 2: Illustration(Adobe Illustrator, Affinity Designer, sketch)<br/>
+        a. Topic: Redesign the SRMKZILLA logo<br/>
+        b. Size: 1080px by 1080px<br/>
+        c. Export: JPEG format with the source file<br/>
+        d. Bonus: Sign off your artwork with your own custom logo<br/>
+      <br/>
+      Task 3: UI/UX (Figma, Sketch)<br/>
+        a. Topic: Redesign RECRUITMENT homepage<br/>
+        b. Export: JPEG format with the source file<br/>
+        c. Bonus: Try to include gradient effect in elements.<br/>
       
       
       `,
@@ -383,27 +378,27 @@ export const questionsArray: Array<
       question: `Note: This task is mandatory for 2nd and 3rd-year students however we strongly recommend 1st-year students to give it a shot too.
 
       Please attempt ONE of the three tasks mentioned below:
-      
-      Task 1 (Cinema 4D/Blender/Adobe after effects/Premiere Pro):
-        a. Topic: Create a logo reveal for a tech video
-        b. Composition size: 1920px by 1080px 30 fps
-        c. Export: MP4 with the source file
-        d. Length: 6 sec(Minimum requirement)
-        e. Bonus: Most intros end with the logo on a black or dark background. It’s not a rule, but it stands out better and keeps you in sync with the industry
-      
-      Task 2 (Premiere pro/ Apple final cut pro/ Adobe After Effects):
-        a. Topic: Edit the Sandesh video below (https://www.instagram.com/p/CdniBEIqmh-/)
-        b. Composition size: 1920px by 1080px 30 fps
-        c. Export: MP4 with the source file
-        d. Length: 10 sec (Minimum requirement)
-        e. Bonus: Use of 3D mockups will be highly recommended
-      
+      <br/>
+      Task 1 (Cinema 4D/Blender/Adobe after effects/Premiere Pro):<br/>
+        a. Topic: Create a logo reveal for a tech video<br/>
+        b. Composition size: 1920px by 1080px 30 fps<br/>
+        c. Export: MP4 with the source file<br/>
+        d. Length: 6 sec(Minimum requirement)<br/>
+        e. Bonus: Most intros end with the logo on a black or dark background. It’s not a rule, but it stands out better and keeps you in sync with the industry.<br/>
+      <br/>
+      Task 2 (Premiere pro/ Apple final cut pro/ Adobe After Effects):<br/>
+        a. Topic: Edit the Sandesh video below <a href=https://www.instagram.com/p/CdniBEIqmh-/ target=_blank><u>Sandesh Link</u></a><br/>
+        b. Composition size: 1920px by 1080px 30 fps<br/>
+        c. Export: MP4 with the source file<br/>
+        d. Length: 10 sec (Minimum requirement)<br/>
+        e. Bonus: Use of 3D mockups will be highly recommended<br/>
+      <br/>
       Task 3 (Adobe After Effects):
-        a. Topic: Promo video on Mozofest video: (https://www.instagram.com/reel/CpApkpZjx1r/?igshid=MzRlODBiNWFlZA==)
-        b. Composition size: Instagram story dimension
-        c. Export: MP4 with the source file
-        d. Length: 15 sec (Stated)
-        e. Bonus: Experiment with typography
+        a. Topic: Promo video on Mozofest video: <a href=https://www.instagram.com/reel/CpApkpZjx1r/?igshid=MzRlODBiNWFlZA== target=_blank><u>Mozofest Link</u></a><br/>
+        b. Composition size: Instagram story dimension<br/>
+        c. Export: MP4 with the source file<br/>
+        d. Length: 15 sec (Stated)<br/>
+        e. Bonus: Experiment with typography<br/>
       `,
     },
     {
@@ -413,7 +408,7 @@ export const questionsArray: Array<
     {
       domain: "events",
       question:
-        "You are working with a team member who is not pulling their weight and is causing frustration among other team members. How would you address this issue while maintaining a positive team environment?",
+        "You are working with a team member who is not pulling their weight and is causing frustration among other team members.<br/> How would you address this issue while maintaining a positive team environment?",
     },
   ],
 ];
