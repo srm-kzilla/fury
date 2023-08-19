8147baa9550b905d2cc89fa5ef99f5f118926d75
Saanvi Kumar <113087236+SaanviKumar13@users.noreply.github.com>
2023-08-19 22:21:22 +0530

    chore: add leads emails (#314)

--- api/utils/constants/constants.go
+++ api/utils/constants/constants.go
@@ -8,4 +8,4 @@ const GOOGLE_RECAPTCHA_VERIFY = "https://www.google.com/recaptcha/api/siteverify
 
 var Domains = [7]string{"technical", "editorial", "corporate", "events", "gfx", "vfx", "content"}
 
-var MaintainerEmails = [6]string{"sk0223@srmist.edu.in", "mm5481@srmist.edu.in", "ar8930@srmist.edu.in", "nn6144@srmist.edu.in", "va1221@srmist.edu.in", "uv8428@srmist.edu.in"}
+var MaintainerEmails = [20]string{"sk0223@srmist.edu.in", "mm5481@srmist.edu.in", "ar8930@srmist.edu.in", "nn6144@srmist.edu.in", "va1221@srmist.edu.in", "uv8428@srmist.edu.in","sk0223@srmist.edu.in","ar8930@srmist.edu.in","by3715@srmist.edu.in","nn6144@srmist.edu.in","dv2099@srmist.edu.in","rt5056@srmist.edu.in","ps2058@srmist.edu.in","os9186@srmist.edu.in","am5656@srmist.edu.in","rr4060@srmist.edu.in","ss2662@srmist.edu.in","rv0221@srmist.edu.in","hr3214@srmist.edu.in","na6187@srmist.edu.in",}
