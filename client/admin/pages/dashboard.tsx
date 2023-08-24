0d3e65368bff25833d7a4381e389f78ed20a30d9
CampVamp <ajayramsaravanan4@gmail.com>
2023-08-24 14:58:53 +0530

    feat: Add Logout functionality

--- client/admin/pages/dashboard.tsx
+++ client/admin/pages/dashboard.tsx
@@ -3,20 +3,31 @@ import Head from "next/head";
 import { Applicant } from "@/services/api";
 import nookies from "nookies";
 import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
 interface ApplicantProps {
   data: Applicant[];
 }
 
 export function Dashboard({ data }: ApplicantProps) {
+  const router = useRouter();
+  const handleLogout = () => {
+    nookies.destroy(null, "token");
+    router.push("/");
+  };
   return (
     <div className="w-full bg-kz-grey p-5">
       <Head>
         <title>Admin Dashboard</title>
       </Head>
       <div>
-        <h1 className="text-kz-orange font-body font-bold text-3xl md:text-5xl">
-          Dashboard
-        </h1>
+        <div className=" flex justify-between items-center">
+          <h1 className="text-kz-orange font-body font-bold text-3xl md:text-5xl">
+            Dashboard
+          </h1>
+          <button className="bg-kz-orange text-white text-lg px-4 py-2 my-2 rounded-2xl md:text-xl" onClick={()=>{handleLogout()}}>
+            Logout
+          </button>
+        </div>
         <hr className="text-kz-orange" />
       </div>
       <div className="flex flex-col">
