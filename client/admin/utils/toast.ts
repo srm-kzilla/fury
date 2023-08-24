e79e761c697c11bf04ac889ad55e49816b09bb44
Utkarshini <utkarshininarayan@gmail.com>
2023-08-24 21:28:33 +0530

    fix: border color for toasts

--- client/admin/utils/toast.ts
+++ client/admin/utils/toast.ts
@@ -10,9 +10,19 @@ const toastBaseOptions = {
   position: "top-right" as ToastPosition,
 };
 
+const successToastOptions = {
+  style: {
+    background: "#202020",
+    color: "#ff644e",
+    border: "1px solid #008000",
+    maxWidth: "250px",
+  },
+  position: "top-right" as ToastPosition,
+};
+
 export default {
   show: (message: string, icon: string) =>
     toast(message, { ...toastBaseOptions, icon }),
   error: (message: string) => toast.error(message, toastBaseOptions),
-  success: (message: string) => toast.success(message, toastBaseOptions),
+  success: (message: string) => toast.success(message, successToastOptions),
 };
