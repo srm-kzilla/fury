265c73c5034fcc3612d7011f75191c09ba07f1d6
Utkarshini <utkarshininarayan@gmail.com>
2023-08-24 22:31:34 +0530

    chore: clean code

--- client/admin/utils/toast.ts
+++ client/admin/utils/toast.ts
@@ -1,28 +1,25 @@
-import toast, { ToastPosition } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 const toastBaseOptions = {
   style: {
     background: "#202020",
     color: "#ff644e",
-    border: "1px solid #ff644e",
+    border: "1px solid #fff",
     maxWidth: "250px",
   },
-  position: "top-right" as ToastPosition,
-};
-
-const successToastOptions = {
-  style: {
-    background: "#202020",
-    color: "#ff644e",
-    border: "1px solid #008000",
-    maxWidth: "250px",
-  },
-  position: "top-right" as ToastPosition,
 };
 
 export default {
   show: (message: string, icon: string) =>
     toast(message, { ...toastBaseOptions, icon }),
-  error: (message: string) => toast.error(message, toastBaseOptions),
-  success: (message: string) => toast.success(message, successToastOptions),
+  error: (message: string) =>
+    toast.error(message, {
+      ...toastBaseOptions,
+      style: { ...toastBaseOptions.style, border: "1px solid #ff644e" },
+    }),
+  success: (message: string) =>
+    toast.success(message, {
+      ...toastBaseOptions,
+      style: { ...toastBaseOptions.style, border: "1px solid #008000" },
+    }),
 };
