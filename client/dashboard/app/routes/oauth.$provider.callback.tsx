d523a9899d3f64aacc09b6abf0019aec564cf5e7
Saanvi Kumar <113087236+SaanviKumar13@users.noreply.github.com>
2024-09-14 17:27:28 +0530

    fix: remove console.log
    Co-authored-by: Harsh Patel <harshnpatel9@gmail.com>

--- client/dashboard/app/routes/oauth.$provider.callback.tsx
+++ client/dashboard/app/routes/oauth.$provider.callback.tsx
@@ -23,8 +23,6 @@ export const loader: LoaderFunction = async ({ request }) => {
     } else if (data.error === "use srm mail id") {
       throw json({ message: "Use SRM Email to Register" }, { status: 400 });
     } else {
-      console.error(data.error);
-      console.log(data);
       throw json({ message: "Something went wrong" }, { status: 400 });
     }
   }
