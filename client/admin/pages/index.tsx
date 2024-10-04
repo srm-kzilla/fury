396b567daef52571d6e2917d6f2c4de0698c346a
Harsh Patel <harshnpatel9@gmail.com>
2024-10-04 18:21:31 +0530

    feat: add stats and searching

--- client/admin/pages/index.tsx
+++ client/admin/pages/index.tsx
@@ -1,10 +1,11 @@
 import DisplayCard from "@/components/DisplayCard";
 import Head from "next/head";
-import { Applicant } from "@/types";
+import type { Applicant } from "@/types";
 import nookies from "nookies";
 import jwt_decode from "jwt-decode";
 import { useRouter } from "next/router";
 import type { GetServerSidePropsContext } from "next";
+import { useState } from "react";
 
 interface ApplicantProps {
   applications: Applicant[];
@@ -13,11 +14,47 @@ interface ApplicantProps {
 
 export function Index({ applications, email }: ApplicantProps) {
   const router = useRouter();
+  const [filter, setFilter] = useState("all");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleLogout = async () => {
     nookies.destroy(null, "token");
     await router.push("/");
   };
+
+  const totalApplications = applications.length;
+  const acceptedApplications = applications.filter((app) =>
+    app.application.some((a) => a.status === "accepted"),
+  ).length;
+  const rejectedApplications = applications.filter((app) =>
+    app.application.some((a) => a.status === "rejected"),
+  ).length;
+
+  const pendingApplications = applications.filter((app) =>
+    app.application.some((a) => a.status === "pending"),
+  ).length;
+
+  const filteredApplications = applications.filter((app) => {
+    if (filter !== "all" && !app.application.some((a) => a.status === filter)) {
+      return false;
+    }
+
+    if (searchTerm) {
+      const lowerSearchTerm = searchTerm.toLowerCase();
+      return (
+        app.name.toLowerCase().includes(lowerSearchTerm) ||
+        app.email.toLowerCase().includes(lowerSearchTerm) ||
+        app.regNo.toLowerCase().includes(lowerSearchTerm) ||
+        app.application.some((a) =>
+          a.questions.some((q) =>
+            q.answer.toLowerCase().includes(lowerSearchTerm),
+          ),
+        )
+      );
+    }
+
+    return true;
+  });
 
   return (
     <>
@@ -32,15 +69,60 @@ export function Index({ applications, email }: ApplicantProps) {
             <button
               className="btn text-white bg-kz-orange w-fit self-end"
               onClick={handleLogout}
+              type="button"
             >
               Logout
             </button>
-            <div className="text-lg">Logged in as <span className="text-kz-orange font-bold">{email}</span></div>
+            <div className="text-lg">
+              Logged in as{" "}
+              <span className="text-kz-orange font-bold">{email}</span>
+            </div>
           </div>
         </div>
+        <div className="flex justify-around w-[70vw] mx-auto mb-8 items-center">
+          <div className="text-center">
+            <h2 className="font-bold">Total Applications</h2>
+            <p>{totalApplications}</p>
+          </div>
+          <div className="text-center">
+            <h2 className="font-bold">Accepted</h2>
+            <p>{acceptedApplications}</p>
+          </div>
+          <div className="text-center">
+            <h2 className="font-bold">Rejected</h2>
+            <p>{rejectedApplications}</p>
+          </div>
+          <div className="text-center">
+            <h2 className="font-bold">Pending</h2>
+            <p>{pendingApplications}</p>
+          </div>
+        </div>
+        <div className="flex justify-around w-[70vw] mx-auto mb-8 items-center">
+          <select
+            className="select select-bordered w-full max-w-xs bg-inherit border-2 border-white/50 rounded-md"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="accepted">Accepted</option>
+            <option value="rejected">Rejected</option>
+            <option value="pending">Pending</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Search by names / email / answers..."
+            className="input input-bordered w-full max-w-xs bg-inherit border-2 border-white/50 rounded-md pl-2 "
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
         <div className="flex flex-col w-[70vw] mx-auto">
-          {applications.map((application: Applicant, index) => (
-            <DisplayCard key={index} {...application} index={index} />
+          {filteredApplications.map((application: Applicant, index) => (
+            <DisplayCard
+              key={application._id || index}
+              {...application}
+              index={index}
+            />
           ))}
         </div>
       </div>
@@ -73,7 +155,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       applications,
-      email
+      email,
     },
   };
 }
